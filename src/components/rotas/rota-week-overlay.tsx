2f044f4e8b575c585618db4d73f4695b49d01b39
chrisedwards-gif <chris.edwards@doughreligion.co.uk>
2026-07-23 06:58:41 +0100

    Correct rota role separators for assistive technology

--- src/components/rotas/rota-week-overlay.tsx
+++ src/components/rotas/rota-week-overlay.tsx
@@ -1,4 +1,19 @@
-export {
-  RotaNoryBuilder as RotaWeekOverlay,
-  type RotaNoryBuilderProps as RotaWeekOverlayProps,
+"use client";
+
+import { useEffect } from "react";
+import {
+  RotaNoryBuilder,
+  type RotaNoryBuilderProps,
 } from "@/components/rotas/rota-nory-builder";
+import "./rota-nory-accessibility.css";
+
+export type RotaWeekOverlayProps = RotaNoryBuilderProps;
+
+export function RotaWeekOverlay(props: RotaWeekOverlayProps) {
+  useEffect(() => {
+    document.querySelectorAll<HTMLElement>(".nory-rota__role-row[role='row']")
+      .forEach((element) => element.setAttribute("role", "presentation"));
+  }, []);
+
+  return <RotaNoryBuilder {...props} />;
+}
