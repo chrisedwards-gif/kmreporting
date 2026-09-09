4aba0c62236b8548a24145cdb05a1c1dd5b7b568
chrisedwards-gif <chris.edwards@doughreligion.co.uk>
2026-09-09 14:55:22 +0100

    Restrict weekly labour ingest trigger execution

---
+++ supabase/migrations/20260909135511_restrict_weekly_ingest_trigger_execution.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.ingest_hourly_labour_from_weekly_file() from public, anon, authenticated;
+grant execute on function public.ingest_hourly_labour_from_weekly_file() to service_role;
